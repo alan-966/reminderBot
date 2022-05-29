cad0bd2e07ce946dc6498548210162d82715802e
alan-966 <alan.gorbunov@mail.ru>
2022-05-30 00:56:28 +0300

    add get_weather and fix MySqlite class

--- MySqlite.py
+++ MySqlite.py
@@ -8,63 +8,109 @@ class MySqlite:
 		self.conn = sqlite3.connect(db_name)
 		print('init', db_name)
 
-	def createTable(self, table_name='users'):
-		cur = self.conn.cursor()
-		cur.execute(f"""CREATE TABLE IF NOT EXISTS {table_name}(
-			id integer PRIMARY KEY autoincrement,
-			first_name TEXT NOT NULL,
-			last_name TEXT,
-			patronymic TEXT,
-			birthday TEXT);
-		""")
-		self.conn.commit()
-		cur.close()
-
-	def dropTable(self, table_name='users'):
-		cur = self.conn.cursor()
-		cur.execute(f"DROP TABLE {table_name}")
-		cur.close()
-
-	def delete(self, current_id, table_name='users'):
-		cur = self.conn.cursor()
-		cur.execute(f"DELETE FROM {table_name} WHERE id={current_id};")
-		self.conn.commit()
-		cur.close()
-
-	def getTables(self):
-		cur = self.conn.cursor()
-		cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-		print(cur.fetchall())
-		cur.close()
-
-	def selectAll(self, table_name='users'):
-		cur = self.conn.cursor()
-		cur.execute(f"SELECT * from {table_name}")
-		print(*cur.fetchall(), sep='\n')
-		cur.close()
-
-	def insert(self, users=[], table_name='users'):
-		"""users=[(first_name, last_name, patronymic, birthday), (...)]"""
-		if len(users) == 0:
+	def createTable(self, table_name='birthdays'):
+		try:
+			cur = self.conn.cursor()
+			cur.execute(f"""CREATE TABLE IF NOT EXISTS {table_name}(
+				id integer PRIMARY KEY autoincrement,
+				first_name TEXT NOT NULL,
+				last_name TEXT,
+				patronymic TEXT,
+				birthday TEXT);
+			""")
+			self.conn.commit()
+			cur.close()
+			return True
+		except Exception as ex:
+			print(ex)
 			return False
 
-		cur = self.conn.cursor()
-		cur.executemany(f"""INSERT INTO {table_name}(
-			first_name,
-			last_name,
-			patronymic,
-			birthday
-		) VALUES(?, ?, ?, ?);""", users)
-		self.conn.commit()
-		cur.close()
+	def dropTable(self, table_name='birthdays'):
+		try:
+			cur = self.conn.cursor()
+			cur.execute(f"DROP TABLE {table_name}")
+			cur.close()
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
 
-	def selectBirthdayUsers(self, table_name='users'):
-		today = date.today().strftime('%d.%m.')
-		cur = self.conn.cursor()
-		cur.execute(f"SELECT * FROM {table_name} WHERE birthday LIKE '{today}%'")
-		data = cur.fetchall()
-		cur.close()
-		return data
+	def delete(self, current_id, table_name='birthdays'):
+		try:
+			cur = self.conn.cursor()
+			cur.execute(f"DELETE FROM {table_name} WHERE id={current_id};")
+			self.conn.commit()
+			cur.close()
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+
+	def getTables(self):
+		try:
+			cur = self.conn.cursor()
+			cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+			print(cur.fetchall())
+			cur.close()
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+
+	def selectAll(self, table_name='birthdays'):
+		try:
+			cur = self.conn.cursor()
+			cur.execute(f"SELECT * from {table_name}")
+			print(*cur.fetchall(), sep='\n')
+			cur.close()
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+
+	def insert(self, rows, table_name='birthdays'):
+		"""users=[(first_name, last_name, patronymic, birthday), (...)]"""
+		if type(rows) != list:
+			return False
+		if len(rows) == 0:
+			return False
+		try:
+			cur = self.conn.cursor()
+			cur.executemany(f"""INSERT INTO {table_name}(
+				first_name,
+				last_name,
+				patronymic,
+				birthday
+			) VALUES(?, ?, ?, ?);""", rows)
+			self.conn.commit()
+			cur.close()
+			return True
+		except Exception as ex:
+			print(ex)
+			return False
+
+	def getBirthdayUsers(self, table_name='birthdays'):
+		try:
+			today = date.today().strftime('%d.%m.')
+			cur = self.conn.cursor()
+			cur.execute(f"SELECT * FROM {table_name} WHERE birthday LIKE '{today}%'")
+			data = cur.fetchall()
+			cur.close()
+			sep = ''
+			namesCount = len(data)
+			if namesCount == 0:
+				return False
+			helpWord = 'празднует' if namesCount == 1 else 'празднуют'
+			if namesCount == 2:
+				sep = ' и '
+			else:
+				sep = ', '
+			names = sep.join([i[1] + ' ' + i[2] for i in data])
+
+			return f'Сегодня {names} {helpWord} День рождения!🎂🥳'
+		except Exception as ex:
+			print(ex)
+			return False
 
 	def __del__(self):
 		self.conn.close()
--- main.py
+++ main.py
@@ -1,50 +1,78 @@
-import telebot
-import schedule
 import time
-import config
+import telebot
+from pygismeteo import Gismeteo
+import schedule
+from config import bot_token, clients_list
 from MySqlite import MySqlite
+from datetime import datetime
 
 
-def telegram_bot():
-    bot = telebot.TeleBot(config.token)
+def get_weather(city):
+	try:
+		gm = Gismeteo()
+		city_id = gm.get_id_by_query(city)
+		step3 = gm.step3(city_id, days=1)
+		message = f'Прогноз погоды на сегодня в городе {city}:\n\U0000203C \U0001F447 \U0000203C'
+		count = 0
+		for i in step3:
+			count += 1
+			if count < 3:
+				continue
+			d = datetime.fromisoformat(i.date.local)
+			t = d.strftime('%H:%M')
+			temp = f'{i.temperature.air.c}°C'
+			desc = i.description.full
+			hum = f'{i.humidity.percent}%'
+			pres = f'{i.pressure.mm_hg_atm}'
+			icon = i.icon
+			message += f'\n{t} — Температура: {temp} {desc}, Влажность: {hum}, Давление: {pres}'
+		return message
+	except Exception as ex:
+		return f'Ошибка при получении погоды: {ex}'
 
-    @bot.message_handler(commands=['start'])
-    def start_message(message):
-        print(message.chat.id)
-        bot.send_message(config.test_chat_id, 'Привет Alan')
 
-    bot.polling(none_stop=True)
+def send_message(to=None, message=None):
+	if to and message:
+		bot = telebot.TeleBot(bot_token)
+		bot.send_message(to, message)
+		bot.stop_bot()
+
+
+def catch_new_chat():
+	bot = telebot.TeleBot(bot_token)
+
+	@bot.message_handler(commands=['start'])
+	def start_message(message):
+		print(message.chat.id)
+
+	bot.polling(none_stop=True)
 
 
 def sqlite():
-    testDB = MySqlite('test.db')
-    # users = [
-    #     ('Gamesh', 'Tahohov', 'Vissarionovich', '27.05.2005'),
-    # ]
-    # testDB.insert(users)
-    testDB.selectAll()
-    print()
-    data = testDB.selectBirthdayUsers()
-
-    sep = ''
-    if len(data) == 2:
-        sep = ' и '
-    else:
-        sep = ', '
-    names = [i[1] + ' ' + i[2] for i in data]
-    print(names)
-    print(sep.join(names))
-    # print(*data, sep='\n')
+	db = MySqlite('moyidei.db')
+	data = db.getBirthdayUsers()
+	print(data)
 
 
 def main():
-    sqlite()
-    # schedule.every(5).seconds.do(sqlite)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-    # telegram_bot()
+	for client in clients_list:
+		for action in clients_list[client]:
+			if action == 'weather':
+				params = clients_list[client][action]
+				message = get_weather(params['city'])
+				send_message(client, message)
+			elif action == 'birthday':
+				pass
+
+	# print(get_weather('Владикавказ'))
+	# send_message()
+	# catch_new_chat()
+	# sqlite()
+	# schedule.every(5).seconds.do(asd)
+	# while True:
+	# 	schedule.run_pending()
+	# 	time.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+	main()
